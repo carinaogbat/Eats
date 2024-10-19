83c365eceeb06bd03a30fd3615ac35e25a1f8507
Carina Ogbat <carinawrightogbat@gmail.com>
2024-10-19 10:28:45 -0700

    testing original gemini code for build

--- src/components/Reviews/ReviewSummary.jsx
+++ src/components/Reviews/ReviewSummary.jsx
@@ -2,8 +2,6 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 import { getReviewsByRestaurantId } from "@/src/lib/firebase/firestore.js";
 import { getAuthenticatedAppForUser } from "@/src/lib/firebase/serverApp";
 import { getFirestore } from "firebase/firestore";
-// added for modified call to avoid [GoogleGenerativeAI Error]: Candidate was blocked due to SAFETY
-import { HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
 
 export async function GeminiSummary({ restaurantId }) {
   const { firebaseServerApp } = await getAuthenticatedAppForUser();
@@ -13,31 +11,9 @@ export async function GeminiSummary({ restaurantId }) {
   );
 
   const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-  // modified call to avoid [GoogleGenerativeAI Error]: Candidate was blocked due to SAFETY
-  // from https://ai.google.dev/gemini-api/docs/safety-settings#node.js
-  // ORIGINAL LINE: const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-  const genAIsafety = [
-    {
-      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-      threshold: HarmBlockThreshold.BLOCK_NONE,
-    },
-    {
-      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-      threshold: HarmBlockThreshold.BLOCK_NONE,
-    },
-  ];
-  // categories from: https://ai.google.dev/gemini-api/docs/safety-settings#node.js
-  // HARM_CATEGORY_HARASSMENT, HARM_CATEGORY_HATE_SPEECH, HARM_CATEGORY_SEXUALLY_EXPLICIT, HARM_CATEGORY_DANGEROUS_CONTENT, and HARM_CATEGORY_CIVIC_INTEGRITY
-  
-  const model = genAI.getGenerativeModel(
-    { 
-      model: "gemini-1.5-flash",
-      safetySettings: genAIsafety
-    }
-  );
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
   const reviewSeparator = "@";
-  
   const prompt = `
     Based on the following restaurant reviews, 
     where each review is separated by a '${reviewSeparator}' character, 
@@ -45,8 +21,6 @@ export async function GeminiSummary({ restaurantId }) {
     
     Here are the reviews: ${reviews.map(review => review.text).join(reviewSeparator)}
   `;
-  
-  // const prompt = "Write a story about a magic backpack.";
 
   try {
     const result = await model.generateContent(prompt);
